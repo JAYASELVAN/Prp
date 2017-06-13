c956036b42e49f91a1d9df30e10d6dceaa29f336
JAYASELVAN  A  K <jayaselvanak@gmail.com>
2017-06-13 14:29:43 +0530

    Create Average.java

---
+++ Average.java
@@ -0,0 +1,23 @@
+package logicalBuilding;
+
+import java.util.Scanner;
+
+public class Average {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] a=new int[10];
+		Scanner sc=new Scanner(System.in);
+		int i,sum=0;
+		float avg;
+		for(i=0;i<10;i++){
+			a[i]=sc.nextInt();
+		}
+		for(i=0;i<10;i++){
+			sum=sum+a[i];
+		}
+		avg=((float) sum)/10.0f;
+		System.out.print(avg);
+	}
+
+}

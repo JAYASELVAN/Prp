d51cdf788b518df01de320d9e7d561f70fbb5862
JAYASELVAN  A  K <jayaselvanak@gmail.com>
2017-08-08 20:22:23 +0530

    Create TravellingPNRList.java

---
+++ TravellingPNRList.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Hello {
+
+    public static void main(String[] args) {
+		//Your Code Here
+        int n,s,c;
+        Scanner sc=new Scanner(System.in);
+        n=sc.nextInt();
+        s=sc.nextInt();
+        LinkedList<String> ll=new LinkedList<String>();
+        for(int i=0;i<s;i++){
+            ll.add(sc.next());
+        }
+        c=sc.nextInt();
+        for(int i=0;i<c;i++){
+            ll.remove(sc.next());
+        }
+        Iterator<String> itr=ll.iterator();
+        int count=0;
+        while(itr.hasNext()&&count<s){
+            System.out.println(itr.next());
+            count++;
+        }
+	}
+}
